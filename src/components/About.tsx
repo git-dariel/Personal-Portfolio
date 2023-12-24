84ce76d6b2da92d2ab9c3822d9b112e41ec973b6
Quezelco1Corplan <104293978+git-dariel@users.noreply.github.com>
2023-12-24 21:40:49 +0800

    modifying about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,24 +8,35 @@ export default function About() {
         {"//About"}
       </h2>
       <div className="flex flex-col items-start justify-center h-full ">
-        <p className=" sm:px-12 py-5 text-gray-300">
-          Hello World! <br /> <br /> I'm <strong> Dariel Avila</strong>, your
-          friendly neighborhood aspiring software engineer with a knack for
-          turning web and mobile development into digital magic. As a full-stack
-          developer, I'm not just about writing code – I'm all about crafting
-          seamless and dazzling user experiences. <br /> <br /> I honed my
-          skills as a backend developer at Quezelco Company, where I had the
-          fantastic opportunity to be part of the IT dream team. Working on the
-          nitty-gritty of backend systems, I've become your go-to person for
-          building robust and scalable solutions. <br /> <br /> But hey, it's
-          not just about the code for me. I'm on a perpetual learning spree,
-          staying ahead of the tech curve and soaking in all the cool
-          innovations. Collaborative projects are my playground, and I thrive in
-          teams where creativity and excellence collide. <br /> <br /> So why
-          me? Because I'm not just in it for the tech; I'm in it to make a mark.
-          If you're looking for a developer with passion, skill, and a dash of
-          digital flair, you've found your match. Excited to weave some coding
-          wonders together! Let's make software magic happen! ✨
+        <p className=" sm:px-12 py-2 text-gray-300">Hello World!</p>
+
+        <p className=" sm:px-12 py-2 text-gray-300">
+          I'm <span className="font-bold"> Dariel Avila</span>, your friendly
+          neighborhood aspiring software engineer with a knack for turning web
+          and mobile development into digital magic. As a full-stack developer,
+          I'm not just about writing code – I'm all about crafting seamless and
+          dazzling user experiences.
+        </p>
+
+        <p className=" sm:px-12 py-2 text-gray-300">
+          I honed my skills as a backend developer at Quezelco Company, where I
+          had the fantastic opportunity to be part of the IT dream team. Working
+          on the nitty-gritty of backend systems, I've become your go-to person
+          for building robust and scalable solutions.
+        </p>
+
+        <p className=" sm:px-12 py-2 text-gray-300">
+          But hey, it's not just about the code for me. I'm on a perpetual
+          learning spree, staying ahead of the tech curve and soaking in all the
+          cool innovations. Collaborative projects are my playground, and I
+          thrive in teams where creativity and excellence collide.
+        </p>
+
+        <p className=" sm:px-12 py-2 text-gray-300">
+          So why me? Because I'm not just in it for the tech; I'm in it to make
+          a mark. If you're looking for a developer with passion, skill, and a
+          dash of digital flair, you've found your match. Excited to weave some
+          coding wonders together! Let's make software magic happen! ✨
         </p>
       </div>
     </section>
