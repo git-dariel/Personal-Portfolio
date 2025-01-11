7f38f6b3fd4ff58159e7781648337843fc8c8783
git-dariel <104293978+git-dariel@users.noreply.github.com>
2025-01-11 18:37:16 +0800

    feat(footer): add LinkedIn icon.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,12 @@
-import { GithubIcon, Facebook, Instagram } from "lucide-react";
+import { GithubIcon, Facebook, Instagram, Linkedin } from "lucide-react";
 import Link from "next/link";
 
 export const Footer = () => {
   return (
     <footer className="min-h-[100px] mt-10 w-full px-5 md:px-20 flex flex-col items-center justify-between rounded-t-2xl">
       <div className="flex justify-between gap-4 mt-4 mb-8">
-        <div className="bg-primary text-white p-2 rounded-full sca">
-          <Link
-            href={"https://www.facebook.com/dariel.avila.129"}
-            target="_blank"
-          >
+        <div className="bg-primary text-white p-2 rounded-full">
+          <Link href={"https://www.facebook.com/dariel.avila.129"} target="_blank">
             <Facebook />
           </Link>
         </div>
@@ -19,17 +16,17 @@ export const Footer = () => {
           </Link>
         </div>
         <div className="bg-primary text-white p-2 rounded-full">
-          <Link
-            href={"https://www.instagram.com/_darielavila/"}
-            target="_blank"
-          >
+          <Link href={"https://www.instagram.com/_darielavila/"} target="_blank">
             <Instagram />
           </Link>
         </div>
+        <div className="bg-primary text-white p-2 rounded-full">
+          <Link href={"https://www.linkedin.com/in/darielavila/"} target="_blank">
+            <Linkedin />
+          </Link>
+        </div>
       </div>
-      <h4 className="text-sm sm:text-base text-gray-300 mb-8">
-        Created with ❤️ using Next.js
-      </h4>
+      <h4 className="text-sm sm:text-base text-gray-300 mb-8">Created with ❤️ using Next.js</h4>
     </footer>
   );
 };
